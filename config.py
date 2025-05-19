08e6164e3b37e9705fd50538947e70454fdb81b9
Your Name <you@example.com>
2025-05-19 17:21:02 +0200

    gitignore

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
 TOR_PATH = r"C:\Users\pavel\OneDrive\Desktop\Tor Browser\Browser\TorBrowser\Tor\tor.exe"
 TORRC_PATH = r"C:\Users\pavel\OneDrive\Desktop\Tor Browser\Browser\TorBrowser\Data\Tor\torrc"
-
-#.gitignore
